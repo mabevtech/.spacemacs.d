dc19bc05b1e4141200020a3e3a271efd21f8be04
Marcel <dolly.marcel@gmail.com>
2022-01-28 20:01:45 -0300

    Remove yas-compiled-snippets files
    No need to have them versioned.

--- private/snippets/js2-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `js2-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'js2-mode
-                     '(("fn" "const ${1:fn} = ${2:$(when (string-match-p \",\" yas-text) \"(\")}${2:args}${2:$(when (string-match-p \",\" yas-text) \")\")} => $0;" "const fn = args => ... ;" nil nil nil "/Users/mabo3n/.emacs.d/private/snippets/js2-mode/fn" nil nil)))
-
-
-;;; Do not edit! File generated at Sun Oct 31 02:48:24 2021
