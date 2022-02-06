068e89a1c8ce47c13dc7f06363c4a0745d962222
Marcel <dolly.marcel@gmail.com>
2022-02-05 22:42:37 -0300

    Use custom, line-wise function text objects for elisp
    It seems better this way. I use like this for other languages.
    But they are not lisps. Code is not data.
    
    Maybe I'll regret creating muscle memory for line-wise objects,
    but I'll try.

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -3,6 +3,7 @@
 ;;; Code:
 
 (require 'init-prog)
+(require 'evil-cleverparens-text-objects)
 
 ;;; snippet expansion
 
@@ -11,8 +12,43 @@
 
 ;;; function text objects
 
-(defalias #'mabo3n/evil-inner-emacs-lisp-function #'evil-cp-inner-defun)
-(defalias #'mabo3n/evil-a-emacs-lisp-function     #'evil-cp-a-defun)
+;; Make a/inner resemble word's (text object).
+;; "inner" for lines spanning defun,
+;; "a" for lines spanning defun + surrounding blank lines
+
+(evil-define-text-object mabo3n/evil-inner-emacs-lisp-function
+  (count &optional beg end type)
+  "Like `evil-cp-a-defun' (yes, \"a\" defun) but line-wise."
+  (if (evil-cp--inside-form-p)
+      (let ((bounds (evil-cp--top-level-bounds)))
+        (evil-range (car bounds) (1+ (cdr bounds)) 'line :expanded t))
+    (error "Not inside a sexp")))
+
+(evil-define-text-object mabo3n/evil-a-emacs-lisp-function
+  (count &optional beg end type)
+  "Like `evil-cp-a-defun' but include surrounding blank lines.
+
+Select the top-level form line-wise, and include succeeding blank lines.
+If none is found, include preceding blank lines (if existent)."
+  (save-excursion
+    (if (evil-cp--inside-form-p)
+        (let* ((bounds             (evil-cp--top-level-bounds))
+               (end-of-succeeding-blank-lines
+                (and (goto-char (cdr bounds))
+                     (re-search-forward (rx (1+ bol (0+ space) ?\n)) nil t)))
+               (beg
+                (or (and end-of-succeeding-blank-lines (car bounds))
+                    (progn
+                      (goto-char (car bounds))
+                      (re-search-backward (rx (or bos (not (any space ?\n)))) nil t)
+                      (or (and (string= "" (match-string-no-properties 0))
+                               ;; we're at the beginning of the buffer
+                               (car bounds))
+                          (and (forward-line 1)
+                               (point))))))
+               (end (or end-of-succeeding-blank-lines (cdr bounds))))
+          (evil-range beg end 'line :expanded t))
+     (error "Not inside a sexp"))))
 
 (provide 'init-elisp)
 ;;; init-elisp.el ends here
