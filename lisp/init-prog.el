bbca35e063e0c367e11563515e288d01f0319a9b
Marcel <dolly.marcel@gmail.com>
2022-02-18 21:54:05 -0300

    Update default function text objects bindings
    `mabo3n/bind-function-text-objects-for-current-language` now
    uses the new convention:
    
    af -> a-function -> from first to last character of the function
    aF -> a-FUNCTION -> a-function + surrounding blank lines
    
    And it is NOT called automatically for every prog-mode.

--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -17,49 +17,37 @@
 
 ;;; function text objects
 
-(defvar mabo3n/default-inner-function-text-obj-function #'evil-inner-paragraph
-  "Symbol for the default \"inner function\" text object.")
-(defvar mabo3n/default-a-function-text-obj-function     #'evil-a-paragraph
+(defvar mabo3n/default-a-function-text-obj-function 'evil-inner-paragraph
   "Symbol for the default \"a function\" text object.")
+(defvar mabo3n/default-a-FUNCTION-text-obj-function 'evil-a-paragraph
+  "Symbol for the default \"a FUNCTION\" text object.")
 
 (defun mabo3n/bind-function-text-objects-for-current-language ()
-  "Bind \"if\" and \"af\" to function text objects of current language.
+  "Bind \"af\" and \"aF\" to function text objects of current language.
 
-Try to bind to \"mabo3n/evil-OBJTYPE-LANGUAGE-function\" where OBJTYPE
-is \"inner\" or \"a\", and LANGUAGE is the current mode's programming
-language.
+Try to bind to \"mabo3n/evil-a-<lang>-function\" and
+\"mabo3n/evil-a-<lang>-FUNCTION\" where <lang> is the current mode's
+programming language.
 
-If symbol's function definition is void, bind to variables
-`mabo3n/default-inner-function-text-obj-function' and
-`mabo3n/default-a-function-text-obj-function' instead.
-
-For instance, if current buffer's `major-mode' is `python-mode',
-\"if\" will make the current evil operator operate over
-the text object defined by function `mabo3n/evil-inner-python-function'
-if it is defined, or `mabo3n/default-inner-function-text-obj-function'
-if it is not."
+If the symbol's function definition is void, bind to variables
+`mabo3n/default-a-function-text-obj-function' and
+`mabo3n/default-a-FUNCTION-text-obj-function' respectively."
   (let* ((current-mode     (symbol-name major-mode))
          (current-language (string-trim-right current-mode "-mode"))
-         (inner-text-obj-symbol
+         (text-object-symbol
           (intern (string-join
-                   (list "mabo3n/evil-inner-" current-language "-function"))))
-         (a-text-obj-symbol
+                   (list "mabo3n/evil-a-" current-language "-function"))))
+         (text-OBJECT-symbol
           (intern (string-join
-                   (list "mabo3n/evil-a-"     current-language "-function")))))
-    (unless (fboundp inner-text-obj-symbol)
-      (setq inner-text-obj-symbol mabo3n/default-inner-function-text-obj-function))
-    (unless (fboundp a-text-obj-symbol)
-      (setq a-text-obj-symbol mabo3n/default-a-function-text-obj-function))
-    ;; (message "Binding function text objects:\n  if -> %s\n  af -> %s"
-    ;;          inner-text-obj-symbol
-    ;;          a-text-obj-symbol)
+                   (list "mabo3n/evil-a-" current-language "-FUNCTION")))))
+    (unless (fboundp text-object-symbol)
+      (setq text-object-symbol mabo3n/default-a-function-text-obj-function))
+    (unless (fboundp text-OBJECT-symbol)
+      (setq text-OBJECT-symbol mabo3n/default-a-FUNCTION-text-obj-function))
     (dolist (map `(,evil-operator-state-local-map
                    ,evil-visual-state-local-map))
-       (define-key map (kbd "if") inner-text-obj-symbol)
-       (define-key map (kbd "af") a-text-obj-symbol))))
-
-(add-hook 'prog-mode-hook
-          #'mabo3n/bind-function-text-objects-for-current-language)
+       (define-key map (kbd "af") text-object-symbol)
+       (define-key map (kbd "aF") text-OBJECT-symbol))))
 
 (provide 'init-prog)
 ;;; init-prog.el ends here
