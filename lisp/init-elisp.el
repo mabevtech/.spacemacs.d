2a75eabbf7f566c6e5817b689517d7f0b1db0196
Marcel <dolly.marcel@gmail.com>
2022-02-18 21:57:36 -0300

    Update elisp default function text objects convention
    See parent commit bbca35e06 for details.

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -12,27 +12,17 @@
 
 ;;; function text objects
 
-;; Make a/inner resemble word's (text object).
-;; "inner" for lines spanning defun,
-;; "a" for lines spanning defun + surrounding blank lines
+(defalias 'mabo3n/evil-a-emacs-lisp-function 'evil-cp-a-defun)
 
-(evil-define-text-object mabo3n/evil-inner-emacs-lisp-function
-  (count &optional beg end type)
-  "Like `evil-cp-a-defun' (yes, \"a\" defun) but line-wise."
-  (if (evil-cp--inside-form-p)
-      (let ((bounds (evil-cp--top-level-bounds)))
-        (evil-range (car bounds) (1+ (cdr bounds)) 'line :expanded t))
-    (error "Not inside a sexp")))
-
-(evil-define-text-object mabo3n/evil-a-emacs-lisp-function
+(evil-define-text-object mabo3n/evil-a-emacs-lisp-FUNCTION
   (count &optional beg end type)
   "Like `evil-cp-a-defun' but include surrounding blank lines.
 
 Select the top-level form line-wise, and include succeeding blank lines.
-If none is found, include preceding blank lines (if existent)."
+If none is found, include preceding blank lines (if any)."
   (save-excursion
     (if (evil-cp--inside-form-p)
-        (let* ((bounds             (evil-cp--top-level-bounds))
+        (let* ((bounds (evil-cp--top-level-bounds))
                (end-of-succeeding-blank-lines
                 (and (goto-char (cdr bounds))
                      (re-search-forward (rx (1+ bol (0+ space) ?\n)) nil t)))
@@ -49,6 +39,9 @@ If none is found, include preceding blank lines (if existent)."
                (end (or end-of-succeeding-blank-lines (cdr bounds))))
           (evil-range beg end 'line :expanded t))
      (error "Not inside a sexp"))))
+
+(add-hook 'emacs-lisp-mode-hook
+          'mabo3n/bind-function-text-objects-for-current-language)
 
 (provide 'init-elisp)
 ;;; init-elisp.el ends here
