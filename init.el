mabevtech/.spacemacs.d

commit c6ad1025e1221a889938f256c1392d7a0ba8b39b

Bind quit functions to SPC C-q instead of SPC q
Testing out this.
More than once I've quit Emacs by mistake by pressing SPC q q q.
It seems I can't get rid of the last "q" because the keymap generated
by `map-y-or-n-p` (the prompt function behind `spacemacs/prompt-kill-emacs`)
derives from `query-replace-map` and so inherits the (q . exit) binding.
But in this case instead of stopping it just quits Emacs.

init.el
@@ -339,6 +339,12 @@ before packages are loaded."
     "x r h" #'helm-regexp
     "T B"  #'toggle-scroll-bar)
 
+  ;; SPC C-q for quit menu
+  (spacemacs/declare-prefix "C-q" "Quit")
+  (spacemacs/set-leader-keys
+    "C-q" (key-binding (kbd "SPC q"))
+    "q" nil)
+
   (evil-define-key (list 'insert 'hybrid) markdown-mode-map (kbd "<tab>") #'hippie-expand)
   (evil-define-key (list 'insert 'hybrid) restclient-mode-map (kbd "<tab>") #'hippie-expand)
 

commit 03ed9a9e7cfe90ab826ae97c3e7298f22b244f4e

Fix menu description of SPC C-q
Bug introduced in c6ad1025e.

init.el
@@ -340,10 +340,10 @@ before packages are loaded."
     "T B"  #'toggle-scroll-bar)
 
   ;; SPC C-q for quit menu
-  (spacemacs/declare-prefix "C-q" "Quit")
   (spacemacs/set-leader-keys
     "C-q" (key-binding (kbd "SPC q"))
     "q" nil)
+  (spacemacs/declare-prefix "C-q" "Quit")
 
   (evil-define-key (list 'insert 'hybrid) markdown-mode-map (kbd "<tab>") #'hippie-expand)
   (evil-define-key (list 'insert 'hybrid) restclient-mode-map (kbd "<tab>") #'hippie-expand)