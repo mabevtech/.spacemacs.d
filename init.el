37298873d2a0f71f45c5ff265ff74821bc9392c7
Marcel <dolly.marcel@gmail.com>
2022-05-10 10:48:15 -0300

    Update face 'highlight-parentheses-highlight set automatically

--- init.el
+++ init.el
@@ -416,5 +416,6 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(highlight-parentheses-highlight ((nil (:weight ultra-bold))) t)
  '(outline-8 ((t (:extend t :foreground "thistle" :weight bold)))))
 )
