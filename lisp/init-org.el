ef197f69368ade5a19a790b34e05e54b7d571475
Marcel <dolly.marcel@gmail.com>
2022-06-30 18:04:24 -0300

    Enhance org-roam config
    - Add mising keybinding for `org-roam-node-insert`
    - Display tags when selecting nodes (helm)
    - Define default capture template with dates and tags
    - Turn on `org-roam-db-autosync-mode` by default
    - Automatically update last_modified date on save

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -253,19 +253,56 @@ inside the `org-capture' buffer with `org-capture-refile')."
 (add-hook 'org-capture-mode-hook 'evil-insert-state 1)
 (add-hook 'org-log-buffer-setup-hook #'evil-insert-state)
 
-
 ;; org-roam
 
 (with-eval-after-load 'org-roam
-  (setq org-roam-directory (expand-file-name "~/org/roam/"))
   (global-set-key (kbd "C-c n l") #'org-roam-buffer-toggle)
   (global-set-key (kbd "C-c n f") #'org-roam-node-find)
   (global-set-key (kbd "C-c n g") #'org-roam-graph)
+  (global-set-key (kbd "C-c n i") #'org-roam-node-insert)
   (global-set-key (kbd "C-c n c") #'org-roam-capture)
   (global-set-key (kbd "C-c n a") #'org-roam-alias-add)
   (global-set-key (kbd "C-c n A") #'org-roam-alias-remove)
   (global-set-key (kbd "C-c n t") #'org-roam-tag-add)
-  (global-set-key (kbd "C-c n T") #'org-roam-tag-remove))
+  (global-set-key (kbd "C-c n T") #'org-roam-tag-remove)
+
+  (setq org-roam-directory (expand-file-name "~/org/roam/")
+
+        ;; Only useful displaying tags because I'm using a
+        ;; vertical completion framework (helm)
+        org-roam-node-display-template
+        (concat "${title:*} " (propertize "${tags:20}" 'face 'org-tag))
+
+        org-roam-capture-templates
+        `(("d" "default" plain "\n* TODO roam entry: ${title}%?"
+           :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                              ,(concat "#+title: ${title}\n"
+                                       "#+created:       %U\n"
+                                       "#+last_modified: %U\n"
+                                       "#+filetags:\n"))
+           :unnarrowed t
+           :immediate-finish t)))
+
+  ;; Recommended
+  (org-roam-db-autosync-mode)
+
+  ;; Update "last_modified" date when saving buffer
+  ;; https://org-roam.discourse.group/t/update-a-field-last-modified-at-save/321
+  (defun mabo3n/org-roam-set-time-stamp-vars ()
+    "Set time-stamp variables to auto update last_modified property."
+    (when (derived-mode-p 'org-mode)
+      (require 'time-stamp)
+      (setq-local time-stamp-active t
+                  time-stamp-line-limit 24
+                  time-stamp-start "#\\+last_modified:[ ]*"
+                  time-stamp-end "$"
+                  time-stamp-format "\[%Y-%m-%d %3a %H:%M\]")))
+  (defun mabo3n/org-roam-timestamp-on-save ()
+    "Call `time-stamp' function if in `org-mode'."
+    (when (derived-mode-p 'org-mode)
+      (time-stamp)))
+  (add-hook 'org-mode-hook #'mabo3n/org-roam-set-time-stamp-vars)
+  (add-hook 'before-save-hook #'mabo3n/org-roam-timestamp-on-save))
 
 ;;; misc
 
