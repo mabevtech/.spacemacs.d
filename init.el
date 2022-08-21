mabevtech/.spacemacs.d

commit 67da5d1d8bed6f13dc2b9dfc2660b5fffa806289

Add spacemacs' osx layer when on MacOS
Instead of removing as done in f01500454.

init.el
@@ -12,7 +12,8 @@ This function should only modify configuration layer settings."
    dotspacemacs-configuration-layer-path '()
 
    dotspacemacs-configuration-layers
-   '(
+   `(
+     ,@(when mabo3n/macos 'osx)
      helm
      (org :variables
           org-enable-github-support t

commit 9929be7f56c3c318032c92245466886a828b2854

Fix osx layer in dotspacemacs-configuration-layers
It was not working in MacOS as it was trying to splice a symbol instead of
sequence.
Bug introduced in 67da5d1d8.

init.el
@@ -22,7 +22,7 @@ This function should only modify configuration layer settings."
 
    dotspacemacs-configuration-layers
    `(
-     ,@(when mabo3n/macos 'osx)
+     ,(when mabo3n/macos 'osx)
      helm
      (org :variables
           org-enable-github-support t